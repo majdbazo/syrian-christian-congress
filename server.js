f9d1de862cd09ae7d664a81205c45e19414b9a37
bazoomaj <abazo51@gmail.com>
2026-06-09 13:40:20 +0200

    Fix session cookie on Render by trusting reverse proxy

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(helmet({
   },
 }));
 
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
